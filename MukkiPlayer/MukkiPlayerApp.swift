9dc78414ff1f3c1c079d67a47aeae96860fc2e2e
TobiSchelling <tobias.schelling@icloud.com>
2026-01-02 14:19:52 +0100

    Initial Commit

---
+++ MukkiPlayer/MukkiPlayerApp.swift
@@ -0,0 +1,21 @@
+//
+//  MukkiPlayerApp.swift
+//  MukkiPlayer
+//
+//  Created by Tobias Schelling on 02.01.26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct MukkiPlayerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ MukkiPlayerTests/MukkiPlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MukkiPlayerTests.swift
+//  MukkiPlayerTests
+//
+//  Created by Tobias Schelling on 02.01.26.
+//
+
+import Testing
+@testable import MukkiPlayer
+
+struct MukkiPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
